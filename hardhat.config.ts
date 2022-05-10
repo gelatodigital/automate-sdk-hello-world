12d3fbdda62b131bd8bbfc60406115a6e6a2f5e1
Sylvain Goumy <goumsss@protonmail.com>
2022-05-10 16:24:53 +0200

    chore: initial project config

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+// PLUGINS
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+
+// Process Env Variables
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ALCHEMY_ID = process.env.ALCHEMY_ID;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "mumbai",
+
+  networks: {
+    ropsten: {
+      chainId: 3,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_ID}`,
+    },
+
+    rinkeby: {
+      chainId: 4,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_ID}`,
+    },
+
+    mumbai: {
+      chainId: 80001,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      url: "https://matic-mumbai.chainstacklabs.com", //`https://rpc-mumbai.matic.today`, // https://rpc-mumbai.matic.today
+    },
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
