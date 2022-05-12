83114d87b4506de47c0af5514e4b1416fc5f9987
Sylvain Goumy <goumsss@protonmail.com>
2022-05-12 11:42:42 +0200

    chore: update solidity compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -37,7 +37,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.12",
         settings: {
           optimizer: { enabled: true, runs: 200 },
         },
