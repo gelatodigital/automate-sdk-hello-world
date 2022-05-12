bc5fc606ee8e96a9f49cb697f158135cb93422a9
Sylvain Goumy <goumsss@protonmail.com>
2022-05-12 11:46:59 +0200

    feat: add deploy & create task example

---
+++ examples/deploy-create-task.ts
@@ -0,0 +1,41 @@
+import hre from "hardhat";
+import { GelatoOpsSDK, isGelatoOpsSupported, TaskReceipt, GELATO_ADDRESSES } from "@gelatonetwork/ops-sdk";
+
+async function main() {
+  const chainId = hre.network.config.chainId as number;
+  if (!isGelatoOpsSupported(chainId)) {
+    console.log(`Gelato Ops network not supported (${chainId})`);
+    return;
+  }
+
+  // Init GelatoOpsSDK
+  const [signer] = await hre.ethers.getSigners();
+  const gelatoOps = new GelatoOpsSDK(chainId, signer);
+
+  // Deploying Counter contract
+  const counterFactory = await hre.ethers.getContractFactory("Counter");
+  console.log("Deploying Counter...");
+  const counter = await counterFactory.deploy(GELATO_ADDRESSES[chainId].ops);
+  await counter.deployed();
+  console.log("Counter deployed to:", counter.address);
+
+  // Create Gelato automated ask
+  console.log("Creating Task...");
+  const res: TaskReceipt = await gelatoOps.createTask({
+    execAddress: counter.address,
+    execSelector: counter.interface.getSighash("increaseCount(uint256)"),
+    execData: counter.interface.encodeFunctionData("increaseCount", [42]),
+    execAbi: counter.interface.format("json") as string,
+    interval: 10 * 60, // execute every 10 minutes
+    name: "Automated counter every 10min",
+  });
+  console.log(`Task created, taskId: ${res.taskId} (tx hash: ${res.transactionHash})`);
+  console.log(`> https://app.gelato.network/task/${res.taskId}?chainId=${chainId}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
